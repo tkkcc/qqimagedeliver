1d0207150b91e450f06fe75291e1d5dcbc05729f
bilabila <bilabila@qq.com>
2021-10-05 02:12:30 +0800

    published

---
+++ README.md
@@ -0,0 +1,39 @@
+QQ图像分发
+
+基于QQ机器人的面向手游辅助脚本的推送服务。
+
+- 前期准备：用户在辅助界面中输入用户QQ，用户点击按钮添加QQ机器人为好友。
+- 运行流程：辅助运行结束后截屏 -> 辅助发送图像与用户QQ给服务端 -> 服务端QQ机器人发送图像给用户QQ
+
+
+# 安装服务端
+
+```sh
+npm i -g qqimagedeliver pm2
+qqimagedeliver --help
+pm2 qqimagedeliver --username 789012 --password 5e6147aa5f # QQ机器人的帐号与密码（md5加密结果）
+```
+
+# 辅助发送图像与用户QQ给服务端
+
+以节点精灵为例
+
+```lua
+captureqqimagedeliver = function(info, to)
+  io.open(getDir() .. '/.nomedia', 'w')
+  local img = getDir() .. "/tmp.jpg"
+  capture(img, 30)
+  local req = {
+    url = "http://a1.bilabila.tk:49875",
+    param = {image = base64(img), info = tostring(info), to = tostring(to)},
+    timeout = 20,
+  }
+  httpPost(req)
+end
+captureqqimagedeliver("剩余理智27", 2367739198)
+```
+
+# 其它推送方式
+
+- 邮件：用户难以查看历史推送，难以设置免打扰。
+- 微信公众号（喵提醒）：用户每两天需要向公众号发消息。
---
+++ main.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+const { createClient } = require('oicq')
+const path = require('path')
+const http = require('http')
+const qs = require('querystring')
+
+const default_opt = {
+  port: 49875,
+  maxsize: 1000000,
+}
+const opt = require('minimist')(process.argv.slice(2))
+if (opt.help || !opt.username || !opt.password) {
+  const exe = "qqimagedeliver"
+  console.log(`${exe} [--username ''] [--password ''] [--host ''][--port ${default_opt.port}] [--maxsize ${default_opt.maxsize}]
+${exe} --username 789012 --password 5e6147aa5f # crypto your password by 'echo -n realpassword|md5sum'`)
+  process.exit(1)
+}
+opt.port = parseInt(opt.port || default_opt.port)
+opt.maxsize = parseInt(opt.maxsize || default_opt.maxsize)
+
+const online = () => {
+  const bot = createClient(opt['username'])
+  bot.on('system.login.slider', () => {
+    process.stdin.once('data', (input) => {
+      bot.sliderLogin(input)
+    })
+  })
+  bot.on('system.login.device', () => {
+    bot.logger.info('验证完成后敲击Enter继续..')
+    process.stdin.once('data', () => {
+      bot.login()
+    })
+  })
+  bot.login(opt['password'])
+  return new Promise((resolve) => {
+    bot.on('system.online', () => resolve(bot))
+  })
+}
+const serve = (bot) => {
+  const server = http.createServer(async (req, res) => {
+    if (req.method != 'POST') return
+    res.end()
+    let body = ''
+    req.on('data', (data) => {
+      body += data
+      if (body.length > opt['maxsize']) {
+        console.log('reach body length limit', opt['maxsize'])
+        req.connection.destroy()
+      }
+    })
+    req.on('end', async () => {
+      body = qs.parse(body)
+      if (!body['to'] || (!body['info'] && !body['image'])) return
+      const message = [
+        { type: 'text', data: { text: body['info'] } },
+        { type: 'image', data: { file: 'base64://' + body['image'] } },
+      ]
+      await bot.sendPrivateMsg(body['to'], message)
+    })
+  })
+  server.listen({ port: opt['port'], host: opt['host'] })
+}
+online().then(serve)
